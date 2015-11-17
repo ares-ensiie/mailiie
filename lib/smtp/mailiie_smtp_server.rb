d531fdefb35a1865ff9e7fed380e1083b3583182
Hurter Jonathan <jonathan@hurter.fr>
2015-11-17 22:04:53 +0100

    Mailiie SMTP Server

--- lib/smtp/mailiie_smtp_server.rb
+++ lib/smtp/mailiie_smtp_server.rb
@@ -1,4 +1,4 @@
-class StdoutSmtpServer < MiniSmtpServer
+class MailiieSmtpServer < MiniSmtpServer
 
   def new_message_event(message_hash)
     puts "# New email received:"
@@ -7,6 +7,9 @@ class StdoutSmtpServer < MiniSmtpServer
     puts "--"
     puts "-- " + message_hash[:data].gsub(/\r\n/, "\r\n-- ")
     puts
+    puts message_hash.to_json
+
+    Message.create({from: message_hash[:from], body: message_hash[:data]})
   end
 
 end
--- lib/tasks/smtp.rake
+++ lib/tasks/smtp.rake
@@ -1,9 +1,9 @@
-require "smtp/stdout_smtp_server"
+require "smtp/mailiie_smtp_server"
 namespace :smtp do
   desc "Manage the SMTP server"
   task start: :environment do
     puts "[SMTP] Starting SMTP server ..."
-    server = StdoutSmtpServer.new(2525, "0.0.0.0", 4)
+    server = MailiieSmtpServer.new(2525, "0.0.0.0", 4)
     server.start
     puts "[SMTP] SMTP Server listenning on port 2525 !"
     server.join
