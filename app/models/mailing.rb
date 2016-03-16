5728f7e6ff45573f3ec0f3b0e05c501aea8552e5
Powkachu <pauline.kobersi@gmail.com>
2016-03-16 18:44:04 +0100

    Supprime les inscriptions après suppression d'une mailing liste

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -2,7 +2,7 @@ class Mailing < ActiveRecord::Base
   validates :nom, presence: true, allow_blank: false, uniqueness: true, length: { maximum: 21 }
   validates :mail, presence: true, allow_blank: false, uniqueness: true, length: { maximum: 21 }
   validates :type_mailing, presence: true, allow_blank: false, length: { maximum: 21 }
-  has_many :inscriptions
+  has_many :inscriptions, :dependent => :delete_all
 
   def contains_user_invalide(uid)
     return self.inscriptions.where(:uid => uid).where(:valide => false).count > 0
